3f62474feeca0c85521d9f32ccc5abc57073e7ec
Ricardo Barantini <ricardobarantini@gmail.com>
2025-07-09 13:31:06 -0300

    update install.sh

--- install.sh
+++ install.sh
@@ -9,17 +9,25 @@ ascii="               _           _
 
 echo -e "\n$ascii\n"
 
-ln -sf $(pwd)/config/alacritty ~/.config/alacritty
-ln -sf $(pwd)/config/hypr ~/.config/hypr
-ln -sf $(pwd)/config/nvim ~/.config/nvim
-ln -sf $(pwd)/config/starship/starship.toml ~/.config
-ln -sf $(pwd)/config/waybar ~/.config/waybar
-ln -sf $(pwd)/config/wofi ~/.config/wofi
-ln -sf $(pwd)/config/zsh/.zshrc ~
+soshoku="~/.local/share/soshoku"
+
+ln -sf $soshoku/config/alacritty ~/.config/alacritty
+ln -sf $soshoku/config/hypr ~/.config/hypr
+ln -sf $soshoku/config/nvim ~/.config/nvim
+ln -sf $soshoku/config/starship/starship.toml ~/.config
+ln -sf $soshoku/config/waybar ~/.config/waybar
+ln -sf $soshoku/config/wofi ~/.config/wofi
+ln -sf $soshoku/config/zsh/.zshrc ~
+
+# Copy .desktop files
+mkdir -p ~/.local/share/applications
+cp $soshoku/applications/*.desktop ~/.local/share/applications
 
 # JetBrainsMono Nerd Font
-curl -L -o /tmp/JetBrainsMono.zip https://github.com/ryanoasis/nerd-fonts/releases/download/v3.4.0/JetBrainsMono.zip
-mkdir -p ~/.local/share/fonts
-unzip -o /tmp/JetBrainsMono.zip -d ~/.local/share/fonts/JetBrainsMono
-rm /tmp/JetBrainsMono.zip
-fc-cache -f
+if grep -qi "JetBrainsMono Nerd Font" < <(fc-list); then
+    curl -L -o /tmp/JetBrainsMono.zip https://github.com/ryanoasis/nerd-fonts/releases/download/v3.4.0/JetBrainsMono.zip
+    mkdir -p ~/.local/share/fonts
+    unzip -o /tmp/JetBrainsMono.zip -d ~/.local/share/fonts/JetBrainsMono
+    rm /tmp/JetBrainsMono.zip
+    fc-cache -f
+fi
