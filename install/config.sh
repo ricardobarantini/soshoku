1721642f3a781a4f249811303415a2ec91aff314
Ricardo Barantini <ricardobarantini@gmail.com>
2025-07-16 14:19:37 -0300

    rofi theme for catppuccin

--- install/config.sh
+++ install/config.sh
@@ -1,14 +1,15 @@
 # ln -snf $SOSHOKU_DIR/config/alacritty ~/.config
-ln -snf $SOSHOKU_DIR/config/ghostty ~/.config
-ln -snf $SOSHOKU_DIR/config/hypr ~/.config
-ln -snf $SOSHOKU_DIR/config/nvim ~/.config
-ln -snf $SOSHOKU_DIR/config/starship.toml ~/.config
-ln -snf $SOSHOKU_DIR/config/waybar ~/.config
-ln -snf $SOSHOKU_DIR/config/wofi ~/.config
-ln -snf $SOSHOKU_DIR/config/.zprofile $HOME
-ln -snf $SOSHOKU_DIR/config/.zshrc $HOME
+ln -snf "$SOSHOKU_DIR"/config/ghostty ~/.config
+ln -snf "$SOSHOKU_DIR"/config/hypr ~/.config
+ln -snf "$SOSHOKU_DIR"/config/nvim ~/.config
+ln -snf "$SOSHOKU_DIR"/config/rofi ~/.config
+ln -snf "$SOSHOKU_DIR"/config/starship.toml ~/.config
+ln -snf "$SOSHOKU_DIR"/config/waybar ~/.config
+ln -snf "$SOSHOKU_DIR"/config/wofi ~/.config
+ln -snf "$SOSHOKU_DIR"/config/.zprofile "$HOME"
+ln -snf "$SOSHOKU_DIR"/config/.zshrc "$HOME"
 
 # Copy .desktop files
 mkdir -p ~/.local/share/applications
-cp $SOSHOKU_DIR/applications/hidden/*.desktop ~/.local/share/applications
+cp "$SOSHOKU_DIR"/applications/hidden/*.desktop ~/.local/share/applications
 update-desktop-database ~/.local/share/applications
